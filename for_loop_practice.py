271adc183f9837c49ad40ac1ada7dd1a7dc8e146
rathoreprachi05 <148968395+rathoreprachi05@users.noreply.github.com>
2025-01-04 17:28:31 +0530

    Update for_loop_practice.py

--- for_loop_practice.py
+++ for_loop_practice.py
@@ -54,10 +54,15 @@ print(a, b)'''
 def fibonacci_series(n):
     n1 = 0
     n2 = 1
-     for i in range(1, n, 1):
+    for i in range(1, n+1, 1):
+         print(n1)
          n3 = n1 + n2
          n1 = n2
          n2 = n3
+n = int(input("Enter a number: "))
+fibonacci_series(n)
+         
+         
          
          
 
