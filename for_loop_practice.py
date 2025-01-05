34c91f5bc3182487ca206fbd327de924671e3537
Saurabh Mishra <saurabhmishra.24x@gmail.com>
2025-01-05 17:43:57 +0530

    Update for_loop_practice.py

--- for_loop_practice.py
+++ for_loop_practice.py
@@ -2,30 +2,41 @@
 #-----------------------------------------------------------------
 
 # Basic function to add 10 to the given integer and return it
+
 def Add(n):
     print(n+10)
 n = int(input("Enter a number: "))
+
 Add(n)
 
 #-----------------------------------------------------------------
 
 # Basic function to print next 10 numbers after the given integer
+
 def Add(n):
     for i in range(1, 11, 1):
         print(n+i)
 n = int(input("Enter a number: "))
 
-Add(n)'''
+Add(n)
 
-'''def Add(n):
+#-----------------------------------------------------------------
+
+#Basic function to check is a given number is even or odd
+
+def Add(n):
     if(n % 2 == 0):
         print(n,"is an even number")
     else:
         print(n,"is an odd number")
 n = int(input("Enter a number: "))
-Add(n)'''
+Add(n)
 
-'''def Add(n):
+#-----------------------------------------------------------------
+
+# Function to return the number of odd and even elements in a list
+
+def Add(n):
     evencount = 0
     oddcount = 0
     for i in n:
@@ -36,9 +47,13 @@ Add(n)'''
     return (evencount, oddcount)
 n = list(map(int, input("Enter the elements of list: ").split(" ")))
 a,b = Add(n)
-print(a, b)'''
+print(a, b)
 
-'''def multiples(n):
+#-----------------------------------------------------------------
+
+# Function to return the count of multiples if 3 and 7 in a given list
+
+def multiples(n):
     countof3 = 0
     countof7 = 0
     for i in n:
@@ -49,7 +64,11 @@ print(a, b)'''
     return (countof3, countof7)
 n = list(map(int, input("Enter the elements of the list: ").split(" ")))
 a,b = multiples(n)
-print(a, b)'''
+print(a, b)
+'''
+#-----------------------------------------------------------------
+
+# Function to print first n terms of a fibonacci series
 
 def fibonacci_series(n):
     n1 = 0
