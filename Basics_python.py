44be459844fec5c0e16f7b7319d464445b1d4cc2
rathoreprachi05 <148968395+rathoreprachi05@users.noreply.github.com>
2025-01-07 22:48:04 +0530

    Update Basics_python.py

--- Basics_python.py
+++ Basics_python.py
@@ -1,8 +1,8 @@
 # Create a program to solve a quadratic equation given coefficients a, b, and c.
-a = int(input("Enter the value of a"))
-b = int(input("Enter the value of b"))
-c = int(input("Enter the value of c"))
-x = int(input("Enter the value of x"))
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = int(input("Enter the value of c: "))
+x = int(input("Enter the value of x: "))
 
 Quad_eq = a * a * x + b * x + c
 
