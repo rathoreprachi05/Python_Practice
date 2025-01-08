36a4591239a7a17b5362e1ec246dded7e9691c8c
rathoreprachi05 <148968395+rathoreprachi05@users.noreply.github.com>
2025-01-08 21:26:52 +0530

    Update Basics_python.py

--- Basics_python.py
+++ Basics_python.py
@@ -2,10 +2,20 @@
 a = int(input("Enter the value of a: "))
 b = int(input("Enter the value of b: "))
 c = int(input("Enter the value of c: "))
-x = int(input("Enter the value of x: "))
-
-Quad_eq = a * a * x + b * x + c
-
-print(Quad_eq)
+x1 = (b * -1 + (b * b - 4 * a * c) ** 0.5) / 2 * a
+x2 = (b * -1 - (b * b - 4 * a * c) ** 0.5) / 2 *  a
+print(x1, x2) 
 
 
+# Write a Python program to validate variable names based on Python's naming conventions.
+def checker(variable):
+  valid = True
+  if(variable[0] not in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_"):
+    valid = False
+  for i in variable :
+     if(i not in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_0123456789"):
+       valid = False
+  return valid
+variable = input("Enter variable's name: ")
+answer = checker(variable)
+print(answer)
