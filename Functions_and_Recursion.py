58aa5f5dc9b0fcc0a3bf67663408830ff83d3a8b
rathoreprachi05 <148968395+rathoreprachi05@users.noreply.github.com>
2025-01-18 17:19:23 +0530

    Update Functions_and_Recursion.py

--- Functions_and_Recursion.py
+++ Functions_and_Recursion.py
@@ -1,4 +1,23 @@
 # Write a function to solve the Tower of Hanoi problem for `n` disks.
+
 # Create a recursive function to generate all subsets of a given list (power set).
+
 # Write a function to calculate the greatest common divisor (GCD) of two numbers using the Euclidean algorithm.
+def hcf_finder(a,b):
+  hcf = 1
+  if(a>b):
+    mini = b
+  else:
+    mini = a
+  for i in range(1, mini+1, 1):
+    if(a % i == 0 and b % i == 0):
+      if(i > hcf):
+        hcf = i
+  return hcf
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+
+ans = hcf_finder(a,b)
+print(ans)
+      
 # Implement a recursive function to determine if a string matches a given pattern (basic regex matching).
