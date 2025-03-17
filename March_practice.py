62fbf029f1f9e3709fcd8cabc91571f2f2ad0443
rathoreprachi05 <148968395+rathoreprachi05@users.noreply.github.com>
2025-03-17 22:08:50 +0530

    Update March_practice.py

--- March_practice.py
+++ March_practice.py
@@ -69,6 +69,22 @@ print(n1)
 
 #------
 
+def fibonacci(n):
+    n1 = 0
+    n2 = 1
+    for i in range(1, n+1, 1):
+        print(n1)
+        n3 = n1 + n2
+        n1 = n2
+        n2 = n3
+
+n = int(input("Enter a number: "))
+
+fibonacci(n)
+        
+        
+    
+
 
 
 
