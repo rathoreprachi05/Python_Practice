69c7b9d8df4019289d189160940d19aa9761c9c6
rathoreprachi05 <148968395+rathoreprachi05@users.noreply.github.com>
2025-03-17 21:12:58 +0530

    Update March_practice.py

--- March_practice.py
+++ March_practice.py
@@ -18,6 +18,25 @@ s = input("Enter a symbol: ")
 
 cal1 = calculate(n1, n2, s)
 print(cal1)
+#-------------------------------------------------
+
+def check_perf_no(n):
+    sum1 = 0
+    for i in range(1, n, 1):
+        if(n % i == 0):
+            sum1 += i
+    if(sum1 == n):
+        return True
+
+n = int(input("Enter a number: "))
+
+n1 = check_perf_no(n)
+if(n1):
+    print(n, "is a perfect number")
+else:
+    print(n, "is not a perfect number")
+
+            
 
 
  
