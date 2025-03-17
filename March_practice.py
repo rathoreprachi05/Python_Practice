fd123d8aeb4ee7adff71d084996d8167b6af6504
rathoreprachi05 <148968395+rathoreprachi05@users.noreply.github.com>
2025-03-17 21:36:17 +0530

    Update March_practice.py

--- March_practice.py
+++ March_practice.py
@@ -56,6 +56,19 @@ if(n1):
 else:
     print(n, "is not an armstrong number")
 
+#--------
+
+def find_fact(n):
+    if(n == 0):
+        return  1
+    return (n * find_fact(n-1))
+n = int(input("Enter a number: "))
+
+n1 = find_fact(n)
+print(n1)
+
+
+
         
 
             
