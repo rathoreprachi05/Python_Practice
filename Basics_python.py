58455afeb24fc0999b9e9e1dbd3724c5a787dc0c
Saurabh Mishra <saurabhmishra.24x@gmail.com>
2025-04-09 23:54:34 +0530

    Update Basics_python.py

--- Basics_python.py
+++ Basics_python.py
@@ -11,6 +11,7 @@ print(x1, x2)
 # Write a Python program to validate variable names based on Python's naming conventions.
 
 def checker(variable):
+  
   valid = True
   if(variable[0] not in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_"):
     valid = False
