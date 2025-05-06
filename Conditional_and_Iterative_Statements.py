153f543741c63838ca5e8d7598c8ff0cd8f4d9f6
Saurabh Mishra <saurabhmishra.24x@gmail.com>
2025-05-06 23:25:29 +0530

    Update Conditional_and_Iterative_Statements.py

--- Conditional_and_Iterative_Statements.py
+++ Conditional_and_Iterative_Statements.py
@@ -9,6 +9,7 @@
 n = int(input("Enter a number:"))
 prime_checker(n)
 
+
 #------------------------------------------------------------------------------------------------
 # Create a program that displays the multiplication table for numbers from 1 to 10 using nested loops.
 def mul_table():
