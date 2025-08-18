e12d7c6221ed6c4368a060b41cdba929617c5078
Saurabh Mishra <saurabhmishra.24x@gmail.com>
2025-08-18 22:23:48 +0530

    Update Functions_and_Recursion.py

--- Functions_and_Recursion.py
+++ Functions_and_Recursion.py
@@ -1,6 +1,5 @@
 # Write a function to solve the Tower of Hanoi problem for `n` disks.
 
-
 # Create a recursive function to generate all subsets of a given list (power set).
 
 # Write a function to calculate the greatest common divisor (GCD) of two numbers using the Euclidean algorithm.
