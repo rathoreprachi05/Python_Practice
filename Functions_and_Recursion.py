244701502c6bd059db3ea7d0b4e12afdc38da51f
Saurabh Mishra <saurabhmishra.24x@gmail.com>
2025-09-13 23:39:16 +0530

    Refactor hcf_finder function for clarity
    Fix indentation and formatting issues in hcf_finder function.

--- Functions_and_Recursion.py
+++ Functions_and_Recursion.py
@@ -9,11 +9,9 @@ def hcf_finder(a,b):
   
   hcf = 1
   if(a>b):
-    mini = b
-    
+    mini = b    
   else:
     mini = a
-    
   for i in range(1, mini+1, 1):
     if(a % i == 0 and b % i == 0):
       if(i > hcf):
