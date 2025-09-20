4602ee4c40db732f67fb542f2a8862b94ba343d7
Prachi Rathore <148968395+rathoreprachi05@users.noreply.github.com>
2025-09-20 23:49:00 +0530

    Update Conditional_and_Iterative_Statements.py

--- Conditional_and_Iterative_Statements.py
+++ Conditional_and_Iterative_Statements.py
@@ -41,9 +41,10 @@ collatz_conj(n)
 def permutation(list1):
   if(len(list1) == 1):
     return [list1]
-
   perm = []
+  
   for i in range(0,len(list1),1):
+    
     current = list1[i]
     remaining = list1[0:i] + list1[i+1:] 
     for j in permutation(remaining):
