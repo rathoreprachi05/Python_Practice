c559d16fb06e069924f26038567eb685698ba845
Prachi Rathore <148968395+rathoreprachi05@users.noreply.github.com>
2025-09-24 23:00:01 +0530

    Update Functions_and_Recursion.py

--- Functions_and_Recursion.py
+++ Functions_and_Recursion.py
@@ -1,9 +1,6 @@
 # Write a function to solve the Tower of Hanoi problem for `n` disks.
-
 # Create a recursive function to generate all subsets of a given list (power set).
-
 # Write a function to calculate the greatest common divisor (GCD) of two numbers using the Euclidean algorithm.
-
 #  Write a function to calculate the greatest common divisor (GCD) of two numbers.
 def hcf_finder(a,b):
   
